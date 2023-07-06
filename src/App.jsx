bdf809651706957db565a0bde4fc717e46e35ecf
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-07-06 09:06:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Alert from "./components/Alert.jsx";
+
+function App() {
+    return (
+        <Alert text="Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại."/>
+    );
+}
+
+export default App;
